306e25493053fab9311bfb85b299167fa37b424f
Muzaffer Bulut <bulutmuzafferr@gmail.com>
2024-04-23 09:57:15 +0300

    variables

---
+++ 1_variables_and_data_types/example.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  double a = 3;
+  double b = 4;
+  double c = 5;
+
+  double result = a+b+c;
+
+  print("Birinci kenarı $a, ikinci kenarı $b, üçünücü kenarı $c, çevresi: $result");
+
+  // --------------
+
+  String name = "Muzaffer";
+  int birthYear = 1998;
+  int year = 2024;
+
+  int age = year-birthYear;
+
+  print("Adım $name, $birthYear yılında doğdum ve yaşım $age");
+
+}
---
+++ 1_variables_and_data_types/variables.dart
@@ -0,0 +1,25 @@
+void main(List<String> args) {
+  int age = 25;
+  String name = "Muzaffer";
+  double whetherInAdana = 50.7;
+  bool isStudent = false;
+  print(name);
+  print(age);
+  print(isStudent);
+  print(whetherInAdana);
+
+  // 
+  num weight = 70;
+  print(weight);
+
+  // no data type
+  var year = 2020;
+  print(year);
+
+  var driverLicense = true;
+  print(driverLicense);
+  
+  print("$name is student: $isStudent");
+
+  print(name.length);
+}
